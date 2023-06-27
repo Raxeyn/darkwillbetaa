fdf07445893bdd62f32007f9f053254a22ea98f9
Darkwill <56265569+Raxeyn@users.noreply.github.com>
2023-06-27 10:25:37 +0300

    Update _document.jsx

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -11,12 +11,12 @@ class MyDocument extends Document {
           <meta name="googlebot" content="index,follow" />
           <meta
             name="description"
-            content="How about listening to music without pausing? Create your own playlists with 750+ supported sound platforms and start listening."
+            content="Discordun En Kullanışlı Ve Güncell Botunu Kullanmaya nedersin Darkwill Beta Çogu Sunucu Eksikligini Tamamlayan Bottur s!yardım yazarak ulaşabilirsin "
           />
           <meta name="twitter:card" content="summary" />
-          <meta name="twitter:site" content="@MusicMaker" />
-          <meta name="twitter:creator" content="@MusicMaker" />
-          <meta property="og:url" content="https://musicmaker.vercel.app" />
+          <meta name="twitter:site" content="@darkwillbeta" />
+          <meta name="twitter:creator" content="@darkwillbeta" />
+          <meta property="og:url" content="https://darkwillbeta.vercel.app" />
           <meta property="og:type" content="website" />
           <meta property="og:title" content="MusicMaker" />
           <link
@@ -26,14 +26,14 @@ class MyDocument extends Document {
           />
           <meta
             property="og:description"
-            content="How about listening to music without pausing? Create your own playlists with 750+ supported sound platforms and start listening."
+            content="Discordun En Kullanışlı Ve Güncell Botunu Kullanmaya nedersin Darkwill Beta Çogu Sunucu Eksikligini Tamamlayan Bottur s!yardım yazarak ulaşabilirsin "
           />
           <meta property="og:image" content="/img/logo.jpg" />
-          <meta property="og:image:alt" content="MusicMaker" />
+          <meta property="og:image:alt" content="DarkwillBeta" />
           <meta property="og:locale" content="en_GB" />
           <meta
             property="og:site_name"
-            content="MusicMaker"
+            content="DarkwillBeta"
           />
           <meta name="theme-color" content="#6108dd" />
           <link rel="icon" href="/img/logo.jpg" type="image/x-icon" />
